a3f08627540b85212abcb89cba2915b68e5abf27
Jenie Nam <jenien@uci.edu>
2020-05-12 14:54:10 -0700

    worked on homework

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,22 @@
-// from data.js
-var tableData = data;
+// from data.js 
+//change vars to lets 
+let tableData = data;
+//reference
+let tbody = d3.select("tbody");
+// 
+// Using the UFO dataset provided in the form of an array of JavaScript objects,
+// write code that appends a table to your web page and then adds new rows of data
+// for each UFO sighting.
+function appendTable(tableData) {
+    tableData.forEach((item) => {
+        // Create table row
+        let row = tbody.append("tr");
+        Object.entries(item).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+    
+};
+appendTable(data);
 
-// YOUR CODE HERE!
